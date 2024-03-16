265a79f87c9033f5b52ed7e6f01f00b2dd7df9f0
thefutureseer <danielharrislimos@outlook.com>
2024-03-15 19:06:20 -0700

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
+const { Todo } = require('./models'); // Require the Todo model
 const { authenticate } = require('./util/authMiddleware.js');
-const { typeDefs, resolvers } = require('./graphql');
+const { typeDefs, resolvers } = require('./graphql/index');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -15,7 +17,7 @@ const server = new ApolloServer({
   resolvers,
   context: ({ req }) => {
     const user = req.user || null;
-    return { user };
+    return { user, Todo };
   },
   formatError: (error) => {
     const { message, path } = error;
@@ -28,6 +30,15 @@ async function startApolloServer() {
   await server.start();
 
   const app = express();
+
+  // Enable CORS middleware
+  app.use(cors(
+    {
+    origin: '*', // Allow requests from any origin, or specify your allowed origins
+    methods: ['GET', 'POST'], // Allow GET and POST requests
+    credentials: true, // Allow credentials to be sent with requests (if needed)
+  }
+  ));
 
   app.use(authenticate);
 
