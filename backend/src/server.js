62acc68451874ebe2b09ee5b10b42f8bed7bcf43
thefutureseer <danielharrislimos@outlook.com>
2024-04-20 21:19:21 -0700

    fix context / authenticate

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { Todo, User } = require('./models');
-const { setJsonContentType, errorHandler } = require('./middleware/middleware.js');
-const { authenticate, context } = require('./middleware/authMiddleware.js');
-const { loggingMiddleware, errorHandlingMiddleware } = require('./middleware/logging.js');
-const { typeDefs, resolvers } = require('./graphql/index');
-const cors = require('cors');
+const { typeDefs, resolvers } = require('./graphql/index.js');
+const {contextCreation} = require('./globalContext/context.js');
+const {authenticate} = require('./auth/authMiddleware.js');
+
+// const {errorHandlingMiddleware } = require('./middleware/logging.js');
 const connectToMongoDB = require('./dbconfig/mongoConfig.js');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // Pass user information to context
+  context: ({ req }) => contextCreation({ req }),
   introspection: true,
-  context,
   formatError: (error) => {
     const { message, path } = error;
     return { message, path };
@@ -26,29 +27,19 @@ const server = new ApolloServer({
 async function startApolloServer() {
   try {
     await server.start();
-
+    
     const app = express();
 
     // Middleware
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
-
-    app.use(loggingMiddleware);
-    app.use(errorHandlingMiddleware);
-    app.use(cors());
-    app.use(setJsonContentType);
-    // Error handling middleware
-    app.use(errorHandler);
-
+    app.use(authenticate);
+      
     await connectToMongoDB(); // Connect to MongoDB
 
-    // Authentication middleware
-    app.use(authenticate);
-
+      // Apply Apollo Server middleware to Express app
     server.applyMiddleware({ 
       app,
       path: '/graphql',
-     });
+    });
 
     app.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}/graphql`);
@@ -57,8 +48,8 @@ async function startApolloServer() {
     console.error('Error starting Apollo Server:', error);
     process.exit(1); // Exit the process if an error occurs during startup
   }
-}
+};
 
 startApolloServer().catch((error) => {
     console.error('Error starting Apollo Server:', error);
-  });
+});
